586e00c49ce055dfc897f6899f4b6e34a0ab4297
米正龙 <S20171105142@mac04.local>
2018-06-21 09:48:32 +0800

    Hello Word

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  text621
+//
+//  Created by 米正龙 on 2018/6/21.
+//  Copyright © 2018年 米正龙. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
